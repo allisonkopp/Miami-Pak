ca4cbad5ec0a1aebcf8d2b3009cce40bc0cbf249
jvelez523 <justin.velez0523@gmail.com>
2019-04-27 14:00:24 -0400

    Masterered

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,8 +15,7 @@ const Hero = ({ Hero }) => {
       <div class="column desctext">
         <h1>MiamiPak</h1>
           <h2>
-            New and popular snacks from around the world. Delivered to your
-            door. fdskjhgbsdkhjfbshkdbfkhjsdbhkfbsadhjfbakhjsbdkhjfabsdkhjdfbas.
+          Miami’s food is its staple, a perfect example of the beautiful, colorful melting pot of cultures that make the city come to life. We know from experience that leaving Miami is not easy, and the culture you become accustomed to as a native isn’t found anywhere else. It’s the little things that make Miami, Miami; and with MiamiPak we hope we can bring Miami to you. Something as simple as a cafecito, or adobo seasoning makes all the difference when you’re missing home. Our goal is to support local companies, by bringing their specialties to you. Wherever you may be, you can get a surprise box of Miami flavors delivered right to your door and instantly feel like you’re back home.
           </h2>
       </div>
     </div>
--- src/components/threecol.js
+++ src/components/threecol.js
@@ -1,29 +1,41 @@
 import React, { Component } from "react";
 import "bulma/css/bulma.css";
 import "../App.css";
+import calendar from "../Assets/cal.png";
+import truck from "../Assets/truck.png";
+import snack from "../Assets/snack.png";
+
 import { CSSTransitionGroup } from "react-transition-group";
 
-
-
 const Threecol = ({ threecol }) => {
-    return (
-      <div class="threecol center">
-        <h1>How it works</h1>
-        <div class="columns">
-          <div class="column">
-            <h1>Step 1</h1>
-            <p>ljhbfghkjwrbekhjgbwkehrjgew</p>
-          </div>
-          <div class="column">
-            <h1>Step 2</h1>
-            <p>ljhbfghkjwrbekhjgbwkehrjgew</p>
-          </div>
-          <div class="column">
-            <h1>Step 3</h1>
-            <p>ljhbfghkjwrbekhjgbwkehrjgew</p>
-          </div>
-        </div>
-      </div>
-  );
+ return (
+   <div class="threecol center">
+     <h1>How it works</h1>
+     <div class="columns">
+       <div class="column">
+         <h4 class="topText">Step 1</h4>
+         <img width="200px" src={calendar} />
+         <h2>Start Subscription or Single Purchase</h2>
+
+         <p>Order for yourself or for a friend!</p>
+       </div>
+       <div class="column">
+         <h4 class="topText">Step 2</h4>
+         <img width="200px" src={truck} />
+         <h2>Hand Packed and Shipped in Miami</h2>
+         <p>
+           Your order is packed with love here in Miami and shipped off in less
+           than 24 hours.
+         </p>
+       </div>
+       <div class="column">
+         <h4 class="topText">Step 3</h4>
+         <img width="200px" src={snack} />
+         <h2>Look forward to more snacks!</h2>
+         <p>New Miami Snacks delivered to your door as often as you choose.</p>
+       </div>
+     </div>
+   </div>
+ );
 };
 export default Threecol;
